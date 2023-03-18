fc018e58216ce176a4083d1b0617f0d291f5e1e6
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-03-18 11:50:45 -0700

    new comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,10 +20,9 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
