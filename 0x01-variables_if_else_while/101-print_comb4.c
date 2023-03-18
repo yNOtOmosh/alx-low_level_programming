306fe3aed478ca307d43746902eb1bb27dd5dc2e
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-03-18 10:34:18 -0700

    new comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - execute function
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		for (j = 1 ; j < 10 ; j++)
+		{
+			for (k = 2 ; k < 10 ; k++)
+			{
+				if (i < j && j < k)
+				{
+					 putchar(i + '0');
+					 putchar(j + '0');
+					 putchar(k + '0');
+					 putchar(',');
+					 putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
