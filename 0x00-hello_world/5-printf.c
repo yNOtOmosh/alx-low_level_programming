cbcfa71e0b150935c4bfb869fcdc41ac5d5d8b21
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-03-19 10:14:29 -0700

    sizes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - start point for program execution
+ * main - start execution
  *
- * Return: 0 ends the execution of function
+ * Return: 0 always
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, ");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);	
 }
