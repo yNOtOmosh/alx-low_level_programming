20640e9a73f4216f24c232c2dd541b7ab88474d2
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-03-22 12:53:57 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,7 +10,7 @@ int main(void)
 	int count;
 	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	for (count = 0 ; count < 50 ; count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = fib1 + fib2;
 		printf("%lu", sum);
@@ -21,8 +21,8 @@ int main(void)
 		if (count == 49)
 			printf("\n");
 		else
-			printf(",");
-			printf(" ");
+			printf(", ");
 	}
+
 	return (0);
 }
