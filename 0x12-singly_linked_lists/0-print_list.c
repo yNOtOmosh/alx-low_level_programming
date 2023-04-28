1417596f375beafb1c91f84392bfce75d84684b9
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-04-27 22:49:56 -0700

    updates

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,7 +5,7 @@
  * print_list - prints all the elements of a linked list
  * @h: pointer to the list_t list to print
  *
- * Return: the number of nodes printed
+ * Return: the number of nodes
 */
 
 size_t print_list(const list_t *h)
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,6 +6,7 @@
  * add_node - adds a new node at the beginning of a linked list
  * @head: double pointer to the list_t list
  * @str: new string to add in the node
+ * 	 needs to be duplicated
  *
  * Return: the address of the new element, or NULL if it fails
  */
@@ -24,7 +25,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new->str = strdup(str);
 	new->len = len;
-	new->next = (*head);
+	new->next =(*head);
 	(*head) = new;
 
 	return (*head);
