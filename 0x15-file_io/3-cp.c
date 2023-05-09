e74fca32cd8acbf9e959cd05b7f712d82d5fc55e
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-05-09 13:00:10 -0700

    task 3- a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,7 +35,7 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dpritnf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
